3577d18b4e818b8771174d3e6e33d4e2692baa57
slygames <238937929+slygames@users.noreply.github.com>
2026-01-30 15:45:42 +1300

    added server to_bytes and from_bytes functions

--- server/cmd/main.go
+++ server/cmd/main.go
@@ -3,16 +3,41 @@ package main
 import (
 	"fmt"
 	"server/pkg/packets"
+
+	"google.golang.org/protobuf/proto"
 )
 
 func main() {
 
 	packet := &packets.Packet{
 		SenderId: 69,
-		Msg:      packets.NewId(54),
+		Msg:      packets.NewChat("Hello World!"),
 	}
 
 	fmt.Println(packet)
 
-	//fmt.Println("Hello World!")
+	bytes := to_bytes(packet)
+
+	from_bytes(bytes)
+
+	fmt.Println("Hello World!")
+}
+
+func to_bytes(packet *packets.Packet) []byte {
+	data, err := proto.Marshal(packet)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(data)
+	return data
+}
+
+func from_bytes(bytes []byte) *packets.Packet {
+	packet := &packets.Packet{}
+	err := proto.Unmarshal(bytes, packet)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(packet)
+	return packet
 }
